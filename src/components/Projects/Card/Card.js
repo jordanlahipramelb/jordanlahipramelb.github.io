d5ae7a6fec529a3ac01f317ec34dd36897bef135
Jordan Lahip-Ramelb <jordan.lahipramelb@gmail.com>
2022-09-06 17:44:20 -0700

    changed button colors

--- src/components/Projects/Card/Card.js
+++ src/components/Projects/Card/Card.js
@@ -26,18 +26,16 @@ const Card = ({
 					<FontAwesomeIcon icon={iconTitleImg} /> {title}
 				</h5>
 				<p className="card-text">{description}</p>
-				<div className="btn-group" role="group">
-					<a href={linkGithub} target="_blank">
-						<button type="button" className="btn btn-outline-info">
-							GitHub
-						</button>
-					</a>
-					<a href={linkDemo} target="_blank">
-						<button type="button" className="btn btn-outline-info">
-							Demo
-						</button>
-					</a>
-				</div>
+				<a href={linkGithub} target="_blank">
+					<button type="button" className="btn btn-secondary mx-1">
+						GitHub
+					</button>
+				</a>
+				<a href={linkDemo} target="_blank">
+					<button type="button" className="btn btn-info">
+						Demo
+					</button>
+				</a>
 				<br />
 				<br />
 				<p className="text-muted">{smallText}</p>
